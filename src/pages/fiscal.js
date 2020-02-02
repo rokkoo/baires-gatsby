a041250fafeeb0a0db292a136a9fc2324ba50cce
Alfonso <alfonsolasaguirre@gmail.com>
2020-02-02 19:31:02 +0100

    Fiscal page

---
+++ src/pages/fiscal.js
@@ -0,0 +1,73 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+// Styles
+import { Section, Title, Text, Content } from "./index.style"
+import { List, Point } from "./services.style"
+
+const FiscalPage = () => {
+  const data = useStaticQuery(graphql`
+    # query {
+    #   fiscal: file(relativePath: { eq: "carousel/im3.jpg" }) {
+    #     childImageSharp {
+    #       # Specify a fixed image and fragment.
+    #       # The default width is 400 pixels
+    #       fixed(width: '100%') {
+    #         ...GatsbyImageSharpFixed
+    #       }
+    #     }
+    #   }
+    # }
+    query {
+      fiscal: file(relativePath: { eq: "carousel/im3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Tipos de servicios" />
+      <Img fluid={data.fiscal.childImageSharp.fluid} alt="Servicio fiscal" />
+      <Content>
+        <Section>
+          <Title>Servicio físcal</Title>
+          <Text>
+            Nos ocupamos de todas las obligaciones fiscales de tu negocio o
+            empresa. Debido a la complejidad de la normativa tributaria y cada
+            vez más exigencias en ese ámbito, te ayudamos en la confección y
+            presentación de los modelos necesarios para siempre cumplas con tus
+            obligaciones formales.
+          </Text>
+        </Section>
+        <Section>
+          <Text>
+            En <b>BAIRES ASESORAMIENTO</b> encontrarás la experiencia y
+            confianza que necesitas. Algunos de nuestros servicios son:
+          </Text>
+          <List>
+            <Point>
+              Declaraciones de <b>IVA</b>
+            </Point>
+            <Point>
+              Declaraciones de Retenciones de Personal, Profesionales,
+              Alquileres, Rendimientos de Capital Mobiliario
+            </Point>
+            <Point>Pagos Fraccionados</Point>
+            <Point>Impuesto sobre la Renta de las Personas Físicas</Point>
+            <Point>Impuesto de Sociedades</Point>
+          </List>
+        </Section>
+      </Content>
+    </Layout>
+  )
+}
+
+export default FiscalPage
---
+++ src/pages/services.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import { theme } from "../components/theme"
+
+export const List = styled.ul`
+  list-style-type: disc;
+  color: red;
+  align-self: center;
+  background-color: #0d0c1d0a;
+  border-radius: 6px;
+  margin-top: 10px;
+`
+
+export const Point = styled.li`
+  color: rgba(0, 0, 0, 0.51);
+  padding: 10px 0;
+`
